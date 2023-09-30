128e5ee2d854d6e13f5e5564c8b800e09ca7d3b2
miguel <egomigue@gmx.es>
2023-09-30 16:29:27 -0300

    adapting evaluation

--- src/config.py
+++ src/config.py
@@ -14,12 +14,6 @@ S3_BUCKET_PREFIX = "queplan_insurance/"
 # Load .env file
 load_dotenv(dotenv_path=ENV_PATH)
 
-os.environ['OPENAI_API_KEY'] = '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'
-os.environ["SERPER_API_KEY"] = "601eb132875f9c48e2686ef59909aeb124c1f81d"
-os.environ["AWS_ACCESS_KEY_ID"] = "AKIA2JHUK4EGBAMYAYFY"
-os.environ["AWS_SECRET_ACCESS_KEY"] = "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"
-#os.environ["OPENAI_NAME"] = ""
-
 # Define environment variables
 AWS_ACCESS_KEY_ID = str(os.getenv("AWS_ACCESS_KEY_ID"))
 AWS_SECRET_ACCESS_KEY = str(os.getenv("AWS_SECRET_ACCESS_KEY"))
